93e2eb064a35c76f58d88b24eb53e2e93110e67f
IsaacSatz <isaacsatz@gmail.com>
2013-10-12 15:48:35 -0700

    It makes the robot go foward on half power for 2.0 seconds

---
+++ Robot.cpp
@@ -0,0 +1,37 @@
+#include "WPILib.h"
+#define PORT_DRIVE_VIC_1 5
+#define PORT_DRIVE_VIC_2 10
+#define PORT_DRIVE_VIC_3 2
+#define PORT_DRIVE_VIC_4 1
+
+class MyRobot : public SimpleRobot {
+Victor leftVic1;
+Victor leftVic2;
+Victor rightVic1;
+Victor rightVic2;
+
+public:
+MyRobot() : 
+  leftVic1(PORT_DRIVE_VIC_1),
+  leftVic2(PORT_DRIVE_VIC_2),
+  rightVic1(PORT_DRIVE_VIC_3),
+  rightVic2(PORT_DRIVE_VIC_4)
+{
+  
+}
+
+void Autonomous(){
+//the stuff we want the robot to do during autonomous mode
+  leftVic1.Set(0.5);
+  leftVic2.Set(0.5);
+  rightVic1.Set(-0.5);
+  rightVic2.Set(-0.5);
+  Wait(2.0)
+  leftVic1(0.0);
+  leftVic2(0.0);
+  rightVic1(0.0);
+  rightVic2(0.0);
+}
+};
+
+START_ROBOT_CLASS(MyRobot);
